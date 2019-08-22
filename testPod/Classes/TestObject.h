acbb84702df1899c709ee23133d2a72e904cc214
kong <kong@kongdeMacBook-Pro.local>
2019-08-22 16:59:45 +0800

    init

---
+++ testPod/Classes/TestObject.h
@@ -0,0 +1,18 @@
+//
+//  TestObject.h
+//  FBSnapshotTestCase
+//
+//  Created by kong on 2019/8/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestObject : NSObject
+
++ (void)testFunc;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ testPod/Classes/TestObject.m
@@ -0,0 +1,17 @@
+//
+//  TestObject.m
+//  FBSnapshotTestCase
+//
+//  Created by kong on 2019/8/22.
+//
+
+#import "TestObject.h"
+
+@implementation TestObject
+
++ (void)testFunc {
+    
+    NSLog(@"testFunc call");
+}
+
+@end
